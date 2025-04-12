bb22a9dbd78d24544e2f3a84dfdab84add1e81e3
艺创AIGC <348310633@qq.com>
2025-04-12 18:42:46 +0800

    Delete uniapp directory

--- uniapp/src/mixins/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue'
-import share, { initShareEvent } from './share'
-import theme from './theme'
-import setTitle from './setTitle'
-export function setupMixin(app: App) {
-    app.mixin(share).mixin(theme).mixin(setTitle)
-    initShareEvent()
-}
